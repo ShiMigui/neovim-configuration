53cc72319b78a2d6d8adc5d4e35950f4b3a4a5b2
Miguel Nascimento dos Santos <miguel.nasto@gmail.com>
2025-07-08 17:47:20 -0300

    Add(Treesitter): Parser tool

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ Licensed under the [MIT License](./LICENSE) — feel free to use, modify, or sha
 * **lazy.nvim** – Efficient, modular plugin manager.
 * **neo-tree** – Elegant file explorer.
 * **bufferline** – Tab-like buffer explorer.
+* **treesitter** – Parser generator tool.
 
 > You can easily add or remove languages by editing the [`languages.lua`](./lua/configs/languages.lua) file.
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,51 @@
+local languages = {}
+
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" },
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = languages,
+				auto_install = true,
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+				indent = {
+					enable = true,
+				},
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<CR>",
+						node_incremental = "<TAB>",
+						node_decremental = "<S-TAB>",
+					},
+				},
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true,
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		event = "BufReadPost",
+	},
+	{
+		"nvim-treesitter/playground",
+		cmd = "TSPlaygroundToggle",
+	},
+}
