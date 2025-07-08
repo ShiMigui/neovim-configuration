e93db33380d278df8ea189273115a88f16457852
Miguel Nascimento dos Santos <miguel.nasto@gmail.com>
2025-07-08 18:13:06 -0300

    Add(conform): A formatter tool

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,18 @@
+local formatters = {
+	["_"] = { "prettier" },
+}
+
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = formatters,
+
+			format_on_save = {
+				timeout_ms = 1000,
+				lsp_fallback = true,
+			},
+		})
+	end,
+}
