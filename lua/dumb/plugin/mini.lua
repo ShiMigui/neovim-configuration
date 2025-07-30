a8d193fa42eddee8432d8ca878840d6bd460b89f
Miguel Nascimento <miguel.nasto@gmail.com>
2025-07-30 13:51:51 -0300

    Refactor(oil -> mini.files): Replace oil with mini.files

--- lua/dumb/plugin/mini.lua
+++ lua/dumb/plugin/mini.lua
@@ -1,6 +1,10 @@
 return {
 	"echasnovski/mini.nvim",
 	version = false,
+	lazy = false,
+	keys = {
+		{ "<C-b>", "<cmd>lua MiniFiles.open()<cr>", desc = "Opens MiniFiles window", mode = "n" },
+	},
 	config = function()
 		-- Docs: https://github.com/echasnovski/mini.nvim/blob/main/readmes/
 		require("mini.move").setup()
@@ -13,6 +17,14 @@ return {
 				option_toggle_prefix = [[\]],
 				windows = true,
 				move_with_alt = true,
+			},
+		})
+		require("mini.files").setup({
+			options = {
+				use_as_default_explorer = true,
+			},
+			windows = {
+				preview = true,
 			},
 		})
 	end,
